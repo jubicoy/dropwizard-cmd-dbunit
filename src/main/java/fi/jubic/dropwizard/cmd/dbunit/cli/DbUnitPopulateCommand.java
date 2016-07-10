0d65e9c5e9b54880e74f24f3d642abe202fa7a21
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2016-07-10 14:00:14 +0300

    Added Freemarker based template expansion for dataset.xml
    Current time can be accessed using "t" object in templates. Current
    datetime is provided as t.now variable and t.add/t.sub methods can be used
    to generate relative datetimes. All dates are formatted using timestamp
    format.

--- src/main/java/fi/jubic/dropwizard/cmd/dbunit/cli/DbUnitPopulateCommand.java
+++ src/main/java/fi/jubic/dropwizard/cmd/dbunit/cli/DbUnitPopulateCommand.java
@@ -1,5 +1,8 @@
 package fi.jubic.dropwizard.cmd.dbunit.cli;
 
+import fi.jubic.dropwizard.cmd.dbunit.template.DateObject;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DatabaseConfiguration;
 import io.dropwizard.setup.Bootstrap;
@@ -11,7 +14,15 @@ import org.dbunit.operation.DatabaseOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Vilppu Vuorinen, vilppu.vuorinen@jubic.fi
@@ -19,6 +30,7 @@ import java.io.InputStream;
  */
 class DbUnitPopulateCommand<T extends Configuration> extends AbsDbUnitCommand<T> {
     private static Logger logger = LoggerFactory.getLogger(DbUnitPopulateCommand.class);
+
     //
     // Constructor(s)
     // **************************************************************
@@ -34,6 +46,9 @@ class DbUnitPopulateCommand<T extends Configuration> extends AbsDbUnitCommand<T>
         );
     }
 
+    //
+    // DbUnitCommand impl
+    // **************************************************************
     @Override
     void run (
             Bootstrap<T> bootstrap,
@@ -46,11 +61,33 @@ class DbUnitPopulateCommand<T extends Configuration> extends AbsDbUnitCommand<T>
         InputStream dataSetStream = cl.getResourceAsStream("dataset.xml");
         IDataSet dataSet = new FlatXmlDataSetBuilder()
                 .setMetaDataSetFromDtd(dtdStream)
-                .build(dataSetStream);
+                .build(processStream(dataSetStream));
+
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
 
         dtdStream.close();
         dataSetStream.close();
         logger.info("DB populated");
+    }
+
+    //
+    // Private methods
+    // **************************************************************
+    private InputStream processStream (
+            InputStream is
+    ) throws IOException, TemplateException {
+        Template t = new Template(
+                "dataset",
+                new InputStreamReader(is),
+                new freemarker.template.Configuration(
+                        freemarker.template.Configuration.VERSION_2_3_23
+                )
+        );
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Map<String, Object> model = new HashMap<>();
+        model.put("t", new DateObject(new Date()));
+        t.process(model, new OutputStreamWriter(out));
+        return new ByteArrayInputStream(out.toByteArray());
     }
 }
